9b7b9ef16af43347005290adba88321a34272b9c
PanagiotisPapadeas <el18039@mail.ntua.gr>
2022-12-14 18:24:56 +0200

    javadoc documentation for CountTimer class

--- src/application/CountTimer.java
+++ src/application/CountTimer.java
@@ -2,15 +2,32 @@ package application;
 
 import java.util.concurrent.*;
 
+
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class CountTimer {
 	private int countdownStarter = 20;
 	
-	public CountTimer (int x){
-		countdownStarter = x;
+	/** Creates a timer with the specified seconds
+	 * * @param seconds The timer's seconds to start the countdown.
+	 */
+	public CountTimer (int seconds){
+		countdownStarter = seconds;
 	}
-    
+	/** Check if timer reaches 0
+	 * @return A boolean that is true if it has reached 0.
+	 */
+	public boolean checktimerend() {
+		if (countdownStarter<0) return true;
+		return false;
+	}
+	/** Prints timer's remaining seconds
+	 */
+	public void printtimer() {
+	System.out.println("\nTime remaining: "+countdownStarter);
+	}
+	/** Starts the timer
+	 */
 	public void starttimer(){
         final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
         final Runnable runnable = new Runnable() {
@@ -29,12 +46,4 @@ public class CountTimer {
         };
         scheduler.scheduleAtFixedRate(runnable, 0, 1, SECONDS);
     }
-	
-	public boolean checktimerend() {
-		if (countdownStarter<0) return true;
-		return false;
-	}
-	public void printtimer() {
-	System.out.println("\nTime remaining: "+countdownStarter);
-	}
 }
